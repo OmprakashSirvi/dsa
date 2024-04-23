80f0a1e30c193848779cbf5e3afa7c68af7d2fb4
OmPrakash Sirvi <omprakashsirvi0211@gmail.com>
2024-04-23 18:50:44 +0530

    start TrappingRainwater problem

--- DsaApplication.java
+++ DsaApplication.java
@@ -1,9 +1,10 @@
 import leetcode.LongestConsecutive;
 import leetcode.Mountain;
+import leetcode.TrappingRainwater;
 
 public class DsaApplication {
     public static void main(String[] args) {
-        int[] arr = {0,3,7,2,5,8,4,6,0,1};
-        System.out.println(LongestConsecutive.solve(arr));
+        int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println( TrappingRainwater.solve(arr));
     }
 }
