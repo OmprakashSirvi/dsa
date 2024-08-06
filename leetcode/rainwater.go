4499b6ba3a0a2de1875ab9f9611e6136c02ee9c9
OmPrakash Sirvi <omprakashsirvi0211@gmail.com>
2024-08-06 22:24:37 +0530

    Go project setup

---
+++ leetcode/rainwater.go
@@ -0,0 +1,10 @@
+package leetcode
+
+func RainWaterSolve(arr []int) int {
+	var trappedWater int
+	for index := 0; index < len(arr); index++ {
+
+	}
+
+	return trappedWater
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "dsa/leetcode"
+
+func main() {
+	arr := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	leetcode.RainWaterSolve(arr)
+}
