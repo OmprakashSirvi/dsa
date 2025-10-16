3802ec0f3a8c8c94248e9947d9a8c414d341daa7
OmprakashSirvi <omprakashsirvi0211@gmail.com>
2025-10-17 05:09:21 +0530

    Implement findTheDifference

---
+++ leetcode/findTheDifference.go
@@ -0,0 +1,18 @@
+package leetcode
+
+func findTheDifference(s string, t string) byte {
+	l := make([]int, 26)
+
+	for _, c := range s {
+		l[c-'a']++
+	}
+
+	for _, c := range t {
+		if l[c-'a'] == 0 {
+			return byte(c)
+		}
+		l[c-'a']--
+	}
+
+	return 0
+}
